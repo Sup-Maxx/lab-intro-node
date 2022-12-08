7571894a8e9070056815dcafc67f2a511b7a5f59
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2022-12-08 13:54:36 +0000

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a-b)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos) return this.items[pos]
+    if (pos => this.length) throw new Error ("OutofBounds")
+  }
 
-  max() {}
+  max() { // biggest number inside of the array
+    if (this.items = []) {
+      throw new Error ("EmptySortedList")
+    }
+    else return Math.max(...this.items)
+  }
 
-  min() {}
+  min() { // smallest number inside of the array
+    if (this.items = []) {
+      throw new Error ("EmptySortedList")
+    }
+    else return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    sum = this.items.reduce((acc,cV) => {
+      return acc+cV
+    }, 0)
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList;
